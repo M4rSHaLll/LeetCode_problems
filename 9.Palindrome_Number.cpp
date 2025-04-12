271fdedffd1cac0b54d8b28f65d223330902e0c7
m4rshall <art-kuzm1n@mail.ru>
2025-04-12 10:40:51 +0300

    new

--- 9.Palindrome_Number.cpp
+++ 9.Palindrome_Number.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
-
-
+#include <math.h>
+int lenx(int x)
+{
+    int count = 0;
+    do{
+        x /= 10;
+        count++;
+    }while (x / 10 != 0);
+    return count;
+}
+bool isPalindrome(int x) {
+    while (x != 0){
+        if(lenx(x) == 1)
+            return true;
+        if ( x < 0 || x % 10 != x / pow(10,lenx(x) - 1))
+            return false;
+        
+        x /= 10;
+        x /= pow(10,lenx(x) - 1);
+    }   
+}
 
 int main()
 {
-
+    std::cout << isPalindrome(121);
 }
